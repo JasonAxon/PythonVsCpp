fc9ccc0527fd30f679a4ea0b1f35d0ae8d6b16ed
JasonAxon <cayabyabjason035@gmail.com>
2025-03-30 21:10:58 +0800

    Add files via upload

---
+++ dictionary.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string word;
+
+    cout << "Type a word you want to be defined. -> ";
+    cin >> word;
+
+    if(word == "Also-ran"){
+        cout << "One that is of little importance especially competitively.";
+    }
+    else if(word == "Arson"){
+        cout << "The willful or malicious burning of property (such as a building) especially with criminal or fraudulent intent.";
+    }
+    else if(word == "Bereft"){
+        cout << "Deprived or robbed of the possession or use of something.";
+    }
+    else if(word == "Cinephile"){
+        cout << "A devotee of motion pictures.";
+    }
+    else if(word == "Derange"){
+        cout << "To disturb the operation or functions of.";
+    }
+    else if(word == "Exile"){
+        cout << "The state or a period of forced absence from one's country or home.";
+    }
+    else if(word == "Forsake"){
+        cout << "To renounce or turn away from entirely.";
+    }
+    else if(word == "Gauche"){
+        cout << "Lacking social experience or grace.";
+    }
+    else if(word == "Hollow"){
+        cout << "Having an unfilled or hollowed-out space within.";
+    }
+    else if(word == "Important"){
+        cout << "Marked by or indicative of significant worth or consequence.";
+    }
+    else if(word == "Juxtaposition"){
+        cout << "The act or an instance of placing two or more things side by side often to compare or contrast or to create an interesting effect.";
+    }
+    else if(word == "Kleptocracy"){
+        cout << "Government by those who seek chiefly status and personal gain at the expense of the governed.";
+    }
+    else if(word == "Limerence"){
+        cout << "Involuntary state of intense romantic desire for another person, characterized by intrusive thoughts, obsessive feelings, and a longing for emotional reciprocation.";
+    }
+    else if(word == "Malady"){
+        cout << "An unwholesome or disordered condition.";
+    }
+    else if(word == "Malignant"){
+        cout << "Tending to produce death or deterioration.";
+    }
+    else if(word == "Naive"){
+        cout << "Deficient in worldly wisdom or informed judgment.";
+    }
+    else if(word == "Oppress"){
+        cout << "To crush or burden by abuse of power or authority.";
+    }
+    else if(word == "Paradox"){
+        cout << "One (such as a person, situation, or action) having seemingly contradictory qualities or phases.";
+    }
+    else if(word == "Pique"){
+        cout << "To excite or arouse especially by a provocation, challenge, or rebuff.";
+    }
+    else if(word == "Quaff"){
+        cout << "To drink (a usually alcoholic beverage) heartily or copiously.";
+    }
+    else if(word == "Rendezvous"){
+        cout << "A place appointed for assembling or meeting.";
+    }
+    else if(word == "Soliloquy"){
+        cout << "The act of talking to oneself.";
+    }
+    else if(word == "Somber"){
+        cout << "So shaded as to be dark and gloomy.";
+    }
+    else if(word == "Tapestry"){
+        cout << "A heavy handwoven reversible textile used for hangings, curtains, and upholstery and characterized by complicated pictorial designs.";
+    }
+    else if(word == "Undermine"){
+        cout << "To subvert or weaken insidiously or secretly.";
+    }
+    else if(word == "Unrequited"){
+        cout << "Not reciprocated or returned in kind.";
+    }
+    else if(word == "Verdict"){
+        cout << "The finding or decision of a jury on the matter submitted to it in trial.";
+    }
+    else if(word == "War"){
+        cout << "A state of usually open and declared armed hostile conflict between states or nations.";
+    }
+    else if(word == "Yearn"){
+        cout << "To long persistently, wistfully, or sadly.";
+    }
+    else if(word == "Zone"){
+        cout << "One of the sections of an area or territory created for a particular purpose.";
+    }
+    else{
+        cout << "Word not found.";
+    }
+}
